295d15f8717887fa8c6f00f99f458bb7ae591f53
Vaz <lucasroyale0805@gmail.com>
2021-09-30 21:41:47 -0300

    vUtils

---
+++ src/main/java/projeto/comandos/Vanish.java
@@ -0,0 +1,45 @@
+package projeto.comandos;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Sound;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Vanish implements CommandExecutor {
+    List<Player> lista = new ArrayList<>();
+
+    @Override
+    public boolean onCommand(CommandSender s, Command c, String label, String[] args) {
+        if(!(s instanceof Player)){
+            Bukkit.getConsoleSender().sendMessage("§cEsse comando só pode ser usado por jogadores.");
+            return true;
+        }
+        Player p = (Player) s;
+        if(args.length != 0){
+            p.sendMessage("§cUse apenas /v");
+            return true;
+        }
+        
+        if(lista.contains(p)){
+            for(Player jogadores : Bukkit.getOnlinePlayers()){
+                jogadores.showPlayer(p);
+            }
+            lista.remove(p);
+            p.sendMessage("§cVocê desativou o seu vanish.");
+            p.playSound(p.getLocation(), Sound.VILLAGER_NO, 1.0F, 1.0F);
+            return true;
+        }
+        for (Player jogadores : Bukkit.getOnlinePlayers()){
+            jogadores.hidePlayer(p);
+        }
+        lista.add(p);
+        p.sendMessage("§aVocê está invisivel.");
+        p.playSound(p.getLocation(), Sound.LEVEL_UP, 1.0F, 1.0F);
+        return false;
+    }
+}
---
+++ src/main/java/projeto/entities/WelcomeInventory.java
@@ -0,0 +1,30 @@
+package projeto.entities;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.ItemStack;
+import projeto.utils.Item;
+
+public class WelcomeInventory {
+
+    private Inventory inventory;
+
+    public WelcomeInventory() {
+        inventory = Bukkit.createInventory(null , 9 * 3, "gui");
+        insertItens();
+    }
+
+    private void insertItens() {
+        ItemStack i = new Item(Material.DIAMOND, 1, (short) 0).setName("§aDiamante").getItemStack();
+        ItemStack i2 = new Item(Material.GOLD_INGOT, 1, (short) 0).setName("§aOuro").getItemStack();
+
+        inventory.setItem(12, i);
+        inventory.setItem(14, i2);
+
+    }
+
+    public Inventory getInventory() {
+    return inventory;
+    }
+}
